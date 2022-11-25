3a6734ad80a829886cd2ad226a7d5040019cc111
yxuefeng <yxuefeng@infervision.com>
2022-11-25 17:17:28 +0800

    添加tokyonight主题

--- lua/config/views/lualine.lua
+++ lua/config/views/lualine.lua
@@ -28,8 +28,7 @@ end
 function M.load()
     M.lualine.setup({
         options = {
-            -- theme = "auto",
-            theme = "gruvbox",
+            theme = "auto",
             icons_enabled = true,
             -- component_separators = { left = "", right = "" },
             -- section_separators = { left = "", right = "" },
--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -15,7 +15,8 @@ plugins.basic = {
 
 plugins.theme = {
     { "sainnhe/gruvbox-material", commit = "3711f1f6b7b2b32a668753b0bf8f14685161a037" },
-    -- { "askfiy/starlight" },
+    { "askfiy/starlight" },
+    { "folke/tokyonight.nvim" },
 }
 
 plugins.lsp = {
