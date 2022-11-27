a9836a2523437485627b08d07c437111cba1666c
yxuefeng <yxuefeng@infervision.com>
2022-11-27 14:45:40 +0800

    修改配置

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -15,8 +15,7 @@ plugins.basic = {
 
 plugins.theme = {
     { "sainnhe/gruvbox-material", commit = "3711f1f6b7b2b32a668753b0bf8f14685161a037" },
-    { "askfiy/starlight" },
-    { "folke/tokyonight.nvim" },
+    -- { "askfiy/starlight" },
 }
 
 plugins.lsp = {
